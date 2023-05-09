ccb09743e499aa189b641dcb02c400e44ddc7201
Siddarth <sidarceus@gmail.com>
2023-05-09 14:51:19 -0400

    added SFML and fixed cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 
 #include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
 int main()
 {
-    cout << "RRT Planner" << endl;
-    return 0;
+    cout << "RRT Playground" << endl;
+
+    sf::RenderWindow window(sf::VideoMode(1200, 800), "RRT");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+            	window.close();
+            	return 0; exit(0);
+            }
+        }
+		window.clear();
+        window.display();
+    }
 }
+
